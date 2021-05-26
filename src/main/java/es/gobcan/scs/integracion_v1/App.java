bba428693322c81de261c720c30b54da30942ba1
4lf4n4 <foncho.hi@gmail.com>
2021-05-26 23:22:07 +0100

    nuestro primer commit

---
+++ src/main/java/es/gobcan/scs/integracion_v1/App.java
@@ -0,0 +1,13 @@
+package es.gobcan.scs.integracion_v1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
